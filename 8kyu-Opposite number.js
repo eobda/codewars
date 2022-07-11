a1f4f5207456b2a15668f1d65d9674371820b60f
maddie <73920692+eobda@users.noreply.github.com>
2022-07-11 13:07:34 -0400

    Update 8kyu-Opposite number.js

--- 8kyu-Opposite number.js
+++ 8kyu-Opposite number.js
@@ -1,11 +1,11 @@
 // Very simple, given an integer or a floating-point number, find its opposite.
 
-// original:
-// function opposite(number) {
-//   return 0 - number;
-// }
-
-// refactor:
+original:
 function opposite(number) {
-  return -number;
-}
+   return 0 - number;
+ }
+
+// refactored version:
+// function opposite(number) {
+//   return -number;
+// }
